0bcf66e575ebf686e868821ca521ddd5fce1079f
MorganSeguela <morgan.seguela@gmail.com>
2023-04-21 17:10:01 +0200

    new data viz

--- data_visualisation/result_vote_webapp/app.R
+++ data_visualisation/result_vote_webapp/app.R
@@ -8,6 +8,11 @@
 #
 
 library(shiny)
+
+library(ggplot2)
+library(plotly)
+
+
 source("db_access.R", local=T)
 
 # Define UI for application that draws a histogram
@@ -18,33 +23,31 @@ ui <- fluidPage(
     
     checkboxInput("text_asso", "Vote associated to text ?", value = FALSE),
 
-    sidebarPanel(
+    fluidRow(
         conditionalPanel(
             condition="input.text_asso == true",
-            uiOutput("choice_text_red")
-        ),
-        uiOutput("choice_cont_id")
+            column(4, 
+                   uiOutput("choice_text_red")
+                   )
+               ),
+        column(4, 
+               uiOutput("choice_cont_id")          
+               )
     ),
     
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    mainPanel(
+        plotlyOutput("parlPlot"),
+        conditionalPanel(
+            condition = "input.text_asso == true",
+            htmlOutput("text_desc")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        htmlOutput("cont_desc")
     )
+    
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     output$choice_text_red = renderUI({
         textData = retrieve_text()
         selectInput("text_id",
@@ -58,14 +61,11 @@ server <- function(input, output) {
     
     output$choice_cont_id = renderUI({
         textData = retrieve_text()
-        print("ok")
         contData = retrieve_content("", FALSE)
-        print("lol")
         if(input$text_asso){
-            print(input$text_id)
-            print(textData[input$text_id == textData[,2],1])
             contData = retrieve_content(textData[input$text_id == textData[,2],1], input$text_asso)
         }
+        print(contData)
         selectInput(
             "content_id",
             label = "Choose which content",
@@ -76,17 +76,113 @@ server <- function(input, output) {
     })
     
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$parlPlot = renderPlotly({
+        textData = retrieve_text()
+        contData = retrieve_content("", FALSE)
+        if(input$text_asso){
+            contData = retrieve_content(textData[input$text_id == textData[,2],1], input$text_asso)
+        }
+        seat_data = get_seat()
+        vote_trans = get_vote_val()
+        
+        cont_id_parl = choose_parliament(contData[input$content_id == contData[,2],1])
+        
+        vote_result = get_result(contData[input$content_id == contData[,2],1])
+        
+        remove(textData, contData)
+        
+        print(cont_id_parl)
+        
+        parl_info = get_parl_info(cont_id_parl[,2])
+    
+        cur_seat = seat_data[seat_data$parliament_name == cont_id_parl$pname,]
+        
+        tmp_parl_place = merge(cur_seat, parl_info, by = "seat_id", all.x=TRUE)
+        remove(seat_data, cont_id_parl)
+        gc()
+        
+        cur_vote = merge(tmp_parl_place, vote_result, by = "parliamentarian_id", all.x = TRUE)
+        cur_vote$final_vote_id[is.na(cur_vote$final_vote_id)] = 3
+        
+        cur_vote$parliament_name[is.na(cur_vote$parliamentarian_id)] = "unknown"
+        vote_trans = rbind(vote_trans, c(3, "absent", 0))
+        vote_trans$color = c("blue", "red", "darkgrey", "black")
+        cur_vote = merge(cur_vote, vote_trans, by.x = "final_vote_id", by.y = "vote_id")
+        
+        cur_vote = cur_vote[,c(1:6,8:12,15:17)]
+        cur_vote$final_vote_id = factor(cur_vote$final_vote_id, levels = vote_trans$vote_id)
+        
+        parliament_graph = ggplot(cur_vote) +
+            geom_point(mapping = aes(x=xpos01, y=ypos01, color=final_vote_id)) +
+            scale_color_manual(
+                labels = unique(cur_vote$vote_name),
+                values = unique(cur_vote$color),
+                guide = guide_legend(
+                    title = "vote",
+                    title.position = "top",
+                    title.hjust = 0.5,
+                    title.vjust = 0.1,
+                    direction = "horizontal"
+                )) +
+            theme_void() +
+            theme(legend.position = "bottom")
+        
+        parliament_graph
+        
+        plot_v1 = ggplotly(parliament_graph, originalData = TRUE)
+        
+        to_modify_plot = plotly_build(plot_v1)
+        
+        for (i in c(1:length(to_modify_plot$x$data))) {
+            to_modify_plot$x$data[[i]]$text = paste(cur_vote$p_fullname[cur_vote$final_vote_id == i-1], "<br />",
+                                                    cur_vote$pg_name[cur_vote$final_vote_id == i-1], "<br />", 
+                                                    cur_vote$country_name[cur_vote$final_vote_id == i-1])
+            to_modify_plot$x$data[[i]]$name = unique(cur_vote$vote_name)[i]
+            
+        }
+        
+        to_modify_plot %>%
+            layout(
+                xaxis = list(zeroline = F, showgrid = F),
+                yaxis = list(zeroline = F, showgrid = F),
+                legend = list(orientation = "h",
+                              title = list(side = "top"))
+            )
+    })
+    
+    output$text_desc = renderUI({
+        textData = retrieve_text()
+        cur_text = textData[input$text_id == textData[,2],]
+        remove(textData)
+        HTML(paste("Reference: ", cur_text[1],
+                   "<br />Description:<br />", cur_text[3],
+                   "<br />Available here: ", cur_text[4]))
+    })
+    
+    output$cont_desc = renderUI({
+        textData = retrieve_text()
+        contData = retrieve_content("", FALSE)
+        if(input$text_asso){
+            contData = retrieve_content(textData[input$text_id == textData[,2],1], input$text_asso)
+        }
+        cur_cont = contData[input$content_id == contData[,2],]
+        print(cur_cont)
+        remove(contData)
+        HTML(paste("This vote is about: ", cur_cont[4],
+                   "<br />The minute is available here: ", cur_cont[5]))
     })
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(
+    ui = ui,
+    server = server,
+    onStart = function() {
+        cat("Doing application setup\n")
+        
+        onStop(function() {
+            cat("Doing application cleanup\n")
+            clean_con()
+        })
+    }
+)
--- data_visualisation/result_vote_webapp/db_access.R
+++ data_visualisation/result_vote_webapp/db_access.R
@@ -8,7 +8,7 @@ library(RPostgreSQL)
 setwd("~/Documents/EuropeanParliamentVote/")
 
 config_db <- ConfigParser$new(Sys.getenv(), optionxform = identity)
-config_db$read("../EP_project/data_storage/database.ini")
+config_db$read("database.ini")
 config_db$data$postgresql
 
 drv = dbDriver("PostgreSQL")
@@ -26,24 +26,31 @@ cur_pool = dbPool(
 retrieve_content = function(reference, isText=TRUE){
     new_con = poolCheckout(cur_pool)
     input_text_id = "
-        SELECT DISTINCT(vc.content_id) as id, DATE(vote_time) || ' - ' || LEFT(description, 100) as desc
-        FROM project.vote_content vc, project.votes v
+        SELECT DISTINCT(vc.content_id) as id,
+            DATE(vote_time) || ' - ' || LEFT(description, 100) as desc,
+            vote_time,
+            vc.description,
+            mn.minute_url
+        FROM project.vote_content vc, project.votes v, project.minute mn
         WHERE reference_text IS NULL
-        AND vc.content_id = v.content_id;
+        AND vc.content_id = v.content_id
+        AND vc.minute_id = mn.minute_id
+        ORDER BY vote_time DESC;
     "
     
-    print(isText)
-    print(reference)
-    
-    if(isText && reference != ""){
+    if(isText && !is.na(reference)){
         input_text_id = paste("
-        SELECT DISTINCT(vc.content_id) as id, DATE(vote_time) || ' - ' || LEFT(description, 100) as desc
-        FROM project.vote_content vc, project.votes v
+        SELECT DISTINCT(vc.content_id) as id,
+            DATE(vote_time) || ' - ' || LEFT(description, 100) as desc,
+            vote_time,
+            vc.description,
+            mn.minute_url
+        FROM project.vote_content vc, project.votes v, project.minute mn
         WHERE vc.content_id = v.content_id
-        AND reference_text LIKE \'", reference,"\';", sep="")
+        AND vc.minute_id = mn.minute_id
+        AND reference_text LIKE \'", reference,"\'
+                              ORDER BY vote_time DESC;", sep="")
     }
-    
-    print(input_text_id)
 
     query = sqlInterpolate(new_con, input_text_id)
     resData = dbGetQuery(new_con, input_text_id)
@@ -56,10 +63,13 @@ retrieve_content = function(reference, isText=TRUE){
 retrieve_text = function(){
     new_con = poolCheckout(cur_pool)
     input_text_id = "
-            SELECT DISTINCT(reference) as id, reference || ' - ' || LEFT(description, 120) || '...' as desc
-            FROM project.text t, project.vote_content vc, project.votes v
-            WHERE t.reference = vc.reference
-            AND vc.content_id = v.content_id;
+        SELECT DISTINCT(te.reference) as id,
+            te.reference || ' - ' || LEFT(te.description, 120) || '...' as desc,
+            te.description,
+            te.url
+        FROM project.text te, project.vote_content vc, project.votes v
+        WHERE te.reference = vc.reference_text
+        AND vc.content_id = v.content_id;
     "
     query = sqlInterpolate(new_con, input_text_id)
     resData = dbGetQuery(new_con, input_text_id)
@@ -67,4 +77,96 @@ retrieve_text = function(){
     resData
 }
 
+
+get_seat = function(){
+    new_con = poolCheckout(cur_pool)
+    seat_query = "SELECT *
+                FROM project.parliament_seat_graph
+                WHERE UPPER(use) LIKE 'PARLIAMENTARIAN'
+                ;"
+    query = sqlInterpolate(new_con, seat_query)
+    resData = dbGetQuery(new_con, seat_query)
+    poolReturn(new_con)
+    resData
+}
+
+
+choose_parliament = function(content_id){
+    this_id = 152544
+    
+    if(length(content_id) != 0){
+        this_id = content_id
+    }
+    
+    first_part= "
+        SELECT pl.parliament_name as pname, so.date_sit as max_date_seat, count(so.parliamentarian_id) as nb
+        FROM project.votes v, project.sits_on so, project.seat s, project.parliament pl
+        WHERE content_id = "
+    second_part = "
+        AND v.parliamentarian_id = so.parliamentarian_id
+        AND so.seat_id = s.seat_id
+        AND s.parliament_id = pl.parliament_id
+        GROUP BY pl.parliament_name, so.date_sit
+        ORDER BY nb DESC, max_date_seat DESC
+        LIMIT 1;
+    "
+    choose_query = paste(first_part, this_id, second_part, sep="")
+    new_con = poolCheckout(cur_pool)
+    query = sqlInterpolate(new_con, choose_query)
+    resData = dbGetQuery(new_con, choose_query)
+    poolReturn(new_con)
+    resData
+}
+
+
+get_vote_val = function(){
+    vote_trans_query = "SELECT * FROM project.vote_value;"
+    new_con = poolCheckout(cur_pool)
+    query = sqlInterpolate(new_con, vote_trans_query)
+    resData = dbGetQuery(new_con, vote_trans_query)
+    poolReturn(new_con)
+    resData
+}
+
+
+get_result = function(content_id){
+    this_id = 152544
+    
+    if(length(content_id) != 0){
+        this_id = content_id
+    }
+    
+    result_query = paste("SELECT *
+            FROM project.votes
+            WHERE content_id = ", this_id, ";")
+    new_con = poolCheckout(cur_pool)
+    query = sqlInterpolate(new_con, result_query)
+    resData = dbGetQuery(new_con, result_query)
+    poolReturn(new_con)
+    resData
+}
+
+get_parl_info = function(chosen_date){
+    this_date = "2023-03-13"
+    if(!is.na(chosen_date)){
+        this_date = chosen_date
+    }
+    parl_query = paste("SELECT *
+            FROM project.parliamentarian_info
+            WHERE date_sit = '", chosen_date,"';", sep="")
+    new_con = poolCheckout(cur_pool)
+    query = sqlInterpolate(new_con, parl_query)
+    resData = dbGetQuery(new_con, parl_query)
+    poolReturn(new_con)
+    resData
+}
+
+get_text_info = function(text_ref){
+    
+}
+
+clean_con = function(){
+    print(cur_pool)
+    poolClose(cur_pool)
+}
 
